d1cd141782a3130f166fe3711e10cb02ab5944bd
Sjj1024 <15670339118@163.com>
2023-01-02 18:28:29 +0800

    add:wozhendehenaini

--- src/tasks/auto_tangtang.py
+++ src/tasks/auto_tangtang.py
@@ -3,6 +3,7 @@ import re
 import time
 from urllib import parse
 import requests
+import random
 
 
 def set_cookies(res):
@@ -14,7 +15,7 @@ def set_cookies(res):
 
 
 def get_html(page_url):
-    print("开始获取html")
+    # print("开始获取html")
     global cookie
     payload = {}
     headers = {
@@ -65,7 +66,7 @@ def get_articales():
 
 
 def get_comment_txt(count):
-    print("获取评论内容:")
+    print(f"获取评论内容:{count}")
     txt_list = ["看起来挺骚的", "溜了，评分留下", "典型的大妈脸", "都是猛人", "被吓到了", "还是支持一下",
                 "最近啥情况", "怎么看不了", "感谢大佬", "有磁力的吗", "最近这种有点多啊", "我看到过的", "这也太牛逼了",
                 "感谢大佬的精彩分享", "都是神人", "艺高人胆大", "就是喜欢这样的题材", "这个不太好吧", "也开始拍剧情了",
@@ -74,7 +75,7 @@ def get_comment_txt(count):
                 "不好意思发了", "怎么全是链接错误", "但还是很喜欢", "刺激就行啊", "看不到了", "这些资源", "真不错真不错",
                 "笑死我了哈哈哈", "高手在民间", "多谢老哥分享", "看起来挺不错", "先赞后看", "的确是好内容", "真人想多了",
                 "因为我本纯良", "这个主题不错"]
-    return txt_list[count % len(txt_list)]
+    return txt_list[random.randint(0, len(txt_list)-1)]
 
 
 def post_comm(tid, txt):
@@ -100,6 +101,7 @@ def post_comm(tid, txt):
 
 
 def post_commit(tid, txt):
+    print(f"开始回复评论：{tid} : {txt}")
     url = f"https://zxfdsfdsf.online/forum.php?mod=post&action=reply&fid=95&tid={tid}&extra=page%3D1&replysubmit=yes&infloat=yes&handlekey=fastpost&inajax=1"
     # payload = 'file=&message=%E4%B8%80%E7%9B%B4%E5%8F%91%E7%83%A7&posttime=1672651842&formhash=44a857f9&usesig=&subject=%2B%2B'
     body = {
@@ -123,7 +125,7 @@ def post_commit(tid, txt):
     response = requests.request("POST", url, headers=headers, data=payload)
     html = response.text
     if "回复发布成功" in html:
-        print("评论完成了....")
+        print("回复发布成功, 评论完成了....")
         return True
     else:
         print(response.text)
@@ -152,7 +154,7 @@ def run():
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
         name = "我要舔你"
-        cookie = "cPNj_2132_saltkey=rOLPzl7d; cPNj_2132_lastvisit=1672626661; cPNj_2132_lastfp=bc56b1f21fc3b458ea72f14c0f3faf3d; cPNj_2132_auth=bd89CyurRPnr%2BfyS6wwSb8rETOF6wScsYmUzyWh5pcljDuqh29BhHG%2FOMEUqKCsiqqw5sSMkrhE1a8kPIQmdNr8o7qU; cPNj_2132_lastcheckfeed=417586%7C1672630317; cPNj_2132_sid=0; cPNj_2132_nofavfid=1; PHPSESSID=d9b8dmf8obt01amb25p60ajjhj; cPNj_2132_home_diymode=1; cPNj_2132_secqaaqSANge0=4656.62d216d3633ea45ba3; cPNj_2132_secqaaqSAJsu0=4663.19b63208593b4beec2; cPNj_2132_ulastactivity=1672650622%7C0; cPNj_2132_lastact=1672650802%09home.php%09spacecp; cPNj_2132_lastact=1672651387%09forum.php%09guide; cPNj_2132_sid=0"
+        cookie = "cPNj_2132_saltkey=BZ6j6Q05; cPNj_2132_lastvisit=1672651408; cPNj_2132_atarget=1; cPNj_2132_visitedfid=95; cPNj_2132_sendmail=1; cPNj_2132_lastfp=bc56b1f21fc3b458ea72f14c0f3faf3d; cPNj_2132_ulastactivity=1672655030%7C0; cPNj_2132_auth=d996lSaxnPze8qXzo12cKXhiA%2BD2S6WmZ%2FS8i8ocG8N6GwINi5bObGbiruq5v7jBsX5s982HHNrW9JSQsMsgng7AuHE; cPNj_2132_lastcheckfeed=438345%7C1672655030; cPNj_2132_lip=208.115.243.40%2C1672655030; cPNj_2132_sid=0; cPNj_2132_st_t=438345%7C1672655031%7Caa33adfd508388d901700d97ff3d285d; cPNj_2132_forum_lastvisit=D_95_1672655031; cPNj_2132_smile=1D1; cPNj_2132_lastact=1672655062%09forum.php%09ajax; cPNj_2132_forum_lastvisit=D_95_1672655172; cPNj_2132_lastact=1672655172%09forum.php%09forumdisplay; cPNj_2132_sid=0; cPNj_2132_st_t=438345%7C1672655172%7Ce4afc1d6c40947fb0a6aa820a1431905"
         user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
     else:
         name = sys.argv[1]
