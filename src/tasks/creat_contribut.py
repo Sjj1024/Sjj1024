844ac81cc49a0143bea4a2fce0a163a81469fd9e
songjj <songjj@zetyun.com>
2023-01-10 12:28:46 +0800

    add:some

--- src/tasks/creat_contribut.py
+++ src/tasks/creat_contribut.py
@@ -7,6 +7,17 @@ def update_file():
     # 产生一个随机内容
     num_ran = random.randint(123, 13232523523)
     print(num_ran)
+    import requests
+    url = "https://api.github.com/repos/Sjj1024/Sjj1024/contents/tasks/blog/hello.txt"
+    payload = "{\r\n  \"message\": \"update from INSOMNIA\",\r\n  \"content\": \"Y3JlYXRlIGZpbGUgZnJvbSBJTlNPTU5JQQoK5oiR54ix5L2g54ix\",\r\n  \"sha\":\"57642f5283c98f6ffa75d65e2bf49d05042b4a6d\"\r\n}"
+    headers = {
+        'Accept': 'application/vnd.github+json',
+        'Authorization': 'Bearer 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',
+        'X-GitHub-Api-Version': '2022-11-28',
+        'Content-Type': 'text/plain'
+    }
+    response = requests.request("PUT", url, headers=headers, data=payload)
+    print(response.text)
 
 
 def run():
