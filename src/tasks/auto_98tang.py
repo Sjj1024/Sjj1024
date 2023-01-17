31f672238da93c16c11ae2b8e7204cc7662f8e55
songjj <songjj@zetyun.com>
2023-01-17 09:44:01 +0800

    add:some

--- src/tasks/auto_98tang.py
+++ src/tasks/auto_98tang.py
@@ -1,12 +1,13 @@
 import random
 import re
+import smtplib
 import sys
+from email.mime.text import MIMEText
 from urllib import parse
 import requests
-from src.tasks.base_obj import BaseMessage
 
 
-class TangTang(BaseMessage):
+class TangTang(object):
     def __init__(self):
         self.name = ""
         self.cookie = ""
@@ -212,6 +213,41 @@ class TangTang(BaseMessage):
         # print(response.text)
         if "succeed" in response.text:
             print(f"签到成功")
+
+    def send_email(self, title, msg, email="648133599@qq.com"):
+        content = str(msg)
+        # 163邮箱服务器地址
+        mail_host = "smtp.163.com"
+        # 163用户名
+        mail_user = "lanxingsjj@163.com"
+        # 密码(部分邮箱为授权码)
+        mail_pass = "QULRMYHTUVMHYVGM"
+        # 邮件发送方邮箱地址
+        sender = "lanxingsjj@163.com"
+        # 邮件接受方邮箱地址，注意需要[]包裹，这意味着你可以写多个邮件地址群发
+        receivers = [email]
+        # 设置email信息
+        # 邮件内容设置
+        message = MIMEText(content, 'plain', 'utf-8')
+        # 邮件主题
+        message['Subject'] = title
+        # 发送方信息
+        message['From'] = sender
+        # 接受方信息
+        message['To'] = receivers[0]
+        # 登录并发送邮件
+        try:
+            # 在阿里云上就要改为下面这种，本地和服务器都友好：
+            smtpObj = smtplib.SMTP_SSL(mail_host, 465)
+            # 登录到服务器
+            smtpObj.login(mail_user, mail_pass)
+            # 发送
+            smtpObj.sendmail(sender, receivers, message.as_string())
+            # 退出
+            smtpObj.quit()
+            print('send email success')
+        except smtplib.SMTPException as e:
+            print('send email error', e)  # 打印错误
 
     def get_html(self, page_url):
         print(f"开始获取html: {page_url}")
