90ccd56baf56440f5624ff50da1f0c433790ffd6
songjj <songjj@zetyun.com>
2023-03-27 15:38:46 +0800

    add:some

--- src/tasks/test_waiwang.py
+++ src/tasks/test_waiwang.py
@@ -2,13 +2,13 @@ import requests
 
 url = "https://www.metart.com/"
 
-payload={}
+payload = {}
 headers = {
-  'Upgrade-Insecure-Requests': '1',
-  'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
-  'sec-ch-ua': '"Not_A Brand";v="99", "Google Chrome";v="109", "Chromium";v="109"',
-  'sec-ch-ua-mobile': '?0',
-  'sec-ch-ua-platform': '"macOS"'
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+    'sec-ch-ua': '"Not_A Brand";v="99", "Google Chrome";v="109", "Chromium";v="109"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"macOS"'
 }
 
 response = requests.request("GET", url, headers=headers, data=payload)
