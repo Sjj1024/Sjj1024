c654aca898ae8d9b6c23be8093c93586b8620d59
1024小神 <15670339118@163.com>
2025-07-09 14:10:05 +0800

    update

--- src/tasks/pp_issue.py
+++ src/tasks/pp_issue.py
@@ -24,11 +24,11 @@ def get_issue(keyword):
 1. Use the latest version, re-enter the token or click the sync button to try again.  
 2. Delete the old project and create a new one to repackage and publish.  
 3. Or check the FAQ documentation: https://ppofficial.netlify.app/question  
-4. Or you can join our discussion group: https://ppofficial.netlify.app/exchange
-PakePlus打包失败修复：
-1.使用最新版本，重新填写token或点击同步按钮试试，
-2.删除老项目，重新创建一个新的项目再打包发布试试，
-3.或查看常见问题文档：https://ppofficial.netlify.app/question
+4. Or you can join our discussion group: https://ppofficial.netlify.app/exchange    
+PakePlus打包失败修复：  
+1.使用最新版本，重新填写token或点击同步按钮试试，  
+2.删除老项目，重新创建一个新的项目再打包发布试试，  
+3.或查看常见问题文档：https://ppofficial.netlify.app/question  
 4.或可以加入我们交流群：https://ppofficial.netlify.app/exchange"""
                 create_comments(issue_num, body)
             close_issue(issue_num, "closed")
