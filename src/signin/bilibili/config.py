2b8c32c5628f3ec9c019a7cad28fb2ea3b2d9e32
1024小神 <15670339118@163.com>
2025-10-24 08:26:46 +0800

    udpate

--- src/signin/bilibili/config.py
+++ src/signin/bilibili/config.py
@@ -1,10 +1,10 @@
 config = {
-    "together": True,  # 是否合并发送结果, 不写或 True 时合并发送
+    "together": False,  # 是否合并发送结果, 不写或 True 时合并发送
     "account": [
         {
             "push": "email",  # together 为 True 时失效, 不写不推送
             "email": "648133599@qq.com",
-            "cookie": "_uuid=1056-4C94-59A4-39103B2A9F74E38977infoc; buvid3=A004761B-0722-98B6-DE47-3F7F0CC63A4631114infoc; b_nut=1665200740; hit-dyn-v2=1; i-wanna-go-back=-1; fingerprint=ded88b3cf197e0f213b1f9e974af4987; buvid_fp_plain=undefined; SESSDATA=99028fc1,1682234827,d63b2*a1; bili_jct=bf73d54e0bf695548db4a9cb9e47d541; DedeUserID=405719127; DedeUserID__ckMd5=d59469073af85b9d; nostalgia_conf=-1; buvid_fp=ded88b3cf197e0f213b1f9e974af4987; sid=66ksxnk9; b_ut=5; bsource=search_baidu; CURRENT_FNVAL=4048; rpdid=|(umYuY~mluR0J'uYY)Yl)lJ|; buvid4=BA4D7D5B-20A4-FF98-DAEC-3661712DE6C831114-022100811-mwLDrsa6+FGgRfzMckPQcQ==; PVID=1; innersign=0; b_lsid=7E79C7E7_185144E658E; bp_video_offset_405719127=739758437043273732",
+            "cookie": "buvid3=39CBF8F1-15F7-A1E7-FEF7-2F75D60A9FBF98924infoc; b_nut=1751897498; _uuid=BF65AFC7-105DC-A2A3-5CA7-10FF2757102F81088323infoc; CURRENT_QUALITY=0; buvid_fp=4b868540638619ec9ee1d6b9ea9253b6; buvid4=76BA0DAA-54CE-1D17-7164-51D0625D17F290748-025080122-wvLLBCnxbC3XMDMnmiz5fw%3D%3D; rpdid=|(u))JkRYJ|k0J'u~lR|lmkmu; enable_web_push=DISABLE; home_feed_column=5; b_lsid=5D1102A24_19A118B6003; SESSDATA=9da85d70%2C1776783001%2Ca3f0e%2Aa1CjBXNgM3OPHNsoObZ9mifXF7YIndfJEwvM1zKmbMC1LjVlmD4lpfUw3PU599n7LosdcSVmtKRV81RjUxMm1OTGE0ZkhQRUY2N1ZpQnIxLXJPLXdsbjFGdkNCbXhnbHQxUDFrVjJPMEUtTWJ6NjAybWhoQzNKaHRtU0tHdlR4MHkySkhnYXNFMVZ3IIEC; bili_jct=a6abe3b866b90649392d491ad715cf26; DedeUserID=3632304542976802; DedeUserID__ckMd5=3b593eb55b65bd52; bp_t_offset_3632304542976802=1126967687252017152; theme-tip-show=SHOWED; CURRENT_FNVAL=2000; browser_resolution=1462-181; sid=ppjky0mh; bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NjE0OTAzMDMsImlhdCI6MTc2MTIzMTA0MywicGx0IjotMX0.75S_T6OJLR6m8htIFxowjVtP7eptmpnwTjrFIbgrEpk; bili_ticket_expires=1761490243",
             "options": {
                 "watch": True,  # 每日观看视频
                 "coins": 10,  # 投币个数
